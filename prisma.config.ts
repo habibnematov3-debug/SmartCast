144fde9d8e8aed4c63567e2090e1996ebe3a1ec8
khabib <habibnematov3@gmail.com>
2026-02-14 01:20:55 +0500

    ed

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,9 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    name: "db",
-    provider: "sqlite",
-    url: env("DATABASE_URL") || "file:./dev.db"
+    url: env("DATABASE_URL") || "file:./dev.db",
   },
   migrations: {
-    path: "prisma/migrations"
-  }
+    path: "prisma/migrations",
+  },
 });
